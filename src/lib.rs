23531e8b74d74f625d34861f9c3280b6d31c5ace
Frank Moreno @kranfix <frankmoreno1993@gmail.com>
2024-09-12 13:33:00 -0500

    Penthagon!

--- src/lib.rs
+++ src/lib.rs
@@ -99,6 +99,11 @@ pub async fn run() {
 }
 
 struct State<'a> {
+    // The window must be declared after the surface so
+    // it gets dropped after it as the surface contains
+    // unsafe references to the window's resources.
+    window: &'a Window,
+
     surface: wgpu::Surface<'a>,
     device: wgpu::Device,
     queue: wgpu::Queue,
@@ -109,12 +114,6 @@ struct State<'a> {
     num_vertices: u32,
     index_buffer: wgpu::Buffer,
     num_indices: u32,
-    diffuse_bind_group: wgpu::BindGroup,
-
-    // The window must be declared after the surface so
-    // it gets dropped after it as the surface contains
-    // unsafe references to the window's resources.
-    window: &'a Window,
 }
 
 impl<'a> State<'a> {
@@ -215,7 +214,7 @@ impl<'a> State<'a> {
                 entry_point: "vs_main", // 1.
                 buffers: &[
                     Vertex::desc(), //
-                ], // 2.
+                ],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
             },
             fragment: Some(wgpu::FragmentState {
@@ -267,122 +266,8 @@ impl<'a> State<'a> {
         });
         let num_indices = INDICES.len() as u32;
 
-        let diffuse_bytes = include_bytes!("./happy-tree.png");
-        let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-        let diffuse_rgba = diffuse_image.to_rgba8();
-
-        use image::GenericImageView;
-        let dimensions = diffuse_image.dimensions();
-
-        let texture_size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
-            depth_or_array_layers: 1,
-        };
-        let diffuse_texture = device.create_texture(&wgpu::TextureDescriptor {
-            // All textures are stored as 3D, we represent our 2D texture
-            // by setting depth to 1.
-            size: texture_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            // Most images are stored using sRGB, so we need to reflect that here.
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
-            // COPY_DST means that we want to copy data to this texture
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-            label: Some("diffuse_texture"),
-            // This is the same as with the SurfaceConfig. It
-            // specifies what texture formats can be used to
-            // create TextureViews for this texture. The base
-            // texture format (Rgba8UnormSrgb in this case) is
-            // always supported. Note that using a different
-            // texture format is not supported on the WebGL2
-            // backend.
-            view_formats: &[],
-        });
-
-        queue.write_texture(
-            // Tells wgpu where to copy the pixel data
-            wgpu::ImageCopyTexture {
-                texture: &diffuse_texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            // The actual pixel data
-            &diffuse_rgba,
-            // The layout of the texture
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * dimensions.0),
-                rows_per_image: Some(dimensions.1),
-            },
-            texture_size,
-        );
-
-        // We don't need to configure the texture view much, so let's
-        // let wgpu define it.
-        let diffuse_texture_view =
-            diffuse_texture.create_view(&wgpu::TextureViewDescriptor::default());
-        let diffuse_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            address_mode_u: wgpu::AddressMode::ClampToEdge,
-            address_mode_v: wgpu::AddressMode::ClampToEdge,
-            address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Nearest,
-            mipmap_filter: wgpu::FilterMode::Nearest,
-            ..Default::default()
-        });
-
-        let texture_bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                entries: &[
-                    wgpu::BindGroupLayoutEntry {
-                        binding: 0,
-                        visibility: wgpu::ShaderStages::FRAGMENT,
-                        ty: wgpu::BindingType::Texture {
-                            multisampled: false,
-                            view_dimension: wgpu::TextureViewDimension::D2,
-                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
-                        },
-                        count: None,
-                    },
-                    wgpu::BindGroupLayoutEntry {
-                        binding: 1,
-                        visibility: wgpu::ShaderStages::FRAGMENT,
-                        // This should match the filterable field of the
-                        // corresponding Texture entry above.
-                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
-                        count: None,
-                    },
-                ],
-                label: Some("texture_bind_group_layout"),
-            });
-
-        let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&diffuse_texture_view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&diffuse_sampler),
-                },
-            ],
-            label: Some("diffuse_bind_group"),
-        });
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[&texture_bind_group_layout], // NEW!
-                push_constant_ranges: &[],
-            });
-
         Self {
+            window,
             surface,
             device,
             queue,
@@ -393,8 +278,6 @@ impl<'a> State<'a> {
             num_vertices,
             index_buffer,
             num_indices,
-            diffuse_bind_group,
-            window,
         }
     }
 
@@ -455,13 +338,11 @@ impl<'a> State<'a> {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+            //render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            //render_pass.draw(0..3, 0..1);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
             render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
-
-            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..3, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
@@ -476,13 +357,12 @@ impl<'a> State<'a> {
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
     position: [f32; 3],
-    // color: [f32; 3],
-    tex_coords: [f32; 2],
+    color: [f32; 3],
 }
 
 impl Vertex {
-    const ATTRIBS: [wgpu::VertexAttribute; 3] =
-        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3, 2 => Float32x2];
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
 
     // descriptor
     fn desc() -> wgpu::VertexBufferLayout<'static> {
@@ -510,28 +390,23 @@ impl Vertex {
 const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.0868241, 0.49240386, 0.0],
-        // color: [0.5, 0.0, 0.5],
-        tex_coords: [0.4131759, 0.99240386],
+        color: [0.5, 0.0, 0.5],
     }, // A
     Vertex {
         position: [-0.49513406, 0.06958647, 0.0],
-        // color: [0.5, 0.0, 0.5],
-        tex_coords: [0.0048659444, 0.56958647],
+        color: [0.5, 0.0, 0.5],
     }, // B
     Vertex {
         position: [-0.21918549, -0.44939706, 0.0],
-        // color: [0.5, 0.0, 0.5],
-        tex_coords: [0.28081453, 0.05060294],
+        color: [0.5, 0.0, 0.5],
     }, // C
     Vertex {
         position: [0.35966998, -0.3473291, 0.0],
-        // color: [0.5, 0.0, 0.5],
-        tex_coords: [0.85967, 0.1526709],
+        color: [0.5, 0.0, 0.5],
     }, // D
     Vertex {
         position: [0.44147372, 0.2347359, 0.0],
-        // color: [0.5, 0.0, 0.5],
-        tex_coords: [0.9414737, 0.7347359],
+        color: [0.5, 0.0, 0.5],
     }, // E
 ];
 
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -29,72 +29,61 @@
 //     return vec4<f32>(x, y, 0.5, 1.0);
 // }
 ////////////////////////////////////////////////////////////////
-// struct VertexInput {
-//     @location(0) position: vec3<f32>,
-//     @location(1) vert_pos: vec2<f32>,
-//     @location(2) color: vec3<f32>,
-// };
-
-// struct VertexOutput {
-//     @builtin(position) clip_position: vec4<f32>,
-//     @location(0) color: vec3<f32>,
-// };
-
-// @vertex
-// fn vs_main(
-//     model: VertexInput,
-// ) -> VertexOutput {
-//     var out: VertexOutput;
-//     out.color = model.color;
-//     out.clip_position = vec4<f32>(model.position, 1.0);
-    
-//     let x = f32(1 - i32(in_vertex_index)) * 0.5;
-//     let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
-//     out.vert_pos = vec2<f32>(x, y);
-
-//     return out;
-// }
-
-// // Fragment shader
-
-// @fragment
-// fn fs_main(in: VertexOutput) -> @location(1) vec4<f32> {
-//     let a = 0.5;
-//     let b = 0.866;
-//     let x = a * in.clip_position.x + b * in.clip_position.y;
-//     let y = b * in.clip_position.x + a * in.clip_position.y;
-    
-//     return vec4<f32>(x, y, 0.4, 0.8);
-//     //return vec4<f32>(in.color, 1.0);
-// }
-
-///////////////////////////////////////////////////////
 struct VertexInput {
     @location(0) position: vec3<f32>,
-    @location(1) tex_coords: vec2<f32>,
-}
+    @location(1) color: vec3<f32>,
+};
 
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) tex_coords: vec2<f32>,
-}
+    @location(0) color: vec3<f32>,
+};
 
 @vertex
 fn vs_main(
     model: VertexInput,
 ) -> VertexOutput {
     var out: VertexOutput;
-    out.tex_coords = model.tex_coords;
+    out.color = model.color;
     out.clip_position = vec4<f32>(model.position, 1.0);
     return out;
 }
 
-@group(0) @binding(0)
-var t_diffuse: texture_2d<f32>;
-@group(0) @binding(1)
-var s_diffuse: sampler;
+// Fragment shader
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
+    return vec4<f32>(in.color, 1.0);
 }
+ 
+
+///////////////////////////////////////////////////////
+// struct VertexInput {
+//     @location(0) position: vec3<f32>,
+//     @location(1) tex_coords: vec2<f32>,
+// }
+
+// struct VertexOutput {
+//     @builtin(position) clip_position: vec4<f32>,
+//     @location(0) tex_coords: vec2<f32>,
+// }
+
+// @vertex
+// fn vs_main(
+//     model: VertexInput,
+// ) -> VertexOutput {
+//     var out: VertexOutput;
+//     out.tex_coords = model.tex_coords;
+//     out.clip_position = vec4<f32>(model.position, 1.0);
+//     return out;
+// }
+
+// @group(0) @binding(0)
+// var t_diffuse: texture_2d<f32>;
+// @group(0) @binding(1)
+// var s_diffuse: sampler;
+
+// @fragment
+// fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+//     return textureSample(t_diffuse, s_diffuse, in.tex_coords);
+// }
