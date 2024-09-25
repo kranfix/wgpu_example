d60a975ee724a266e1527b1bd350d999da3ddf4a
Frank Moreno @kranfix <frankmoreno1993@gmail.com>
2024-09-25 17:09:44 -0500

    removed anyhow

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ pub enum RunError {
     EventLoopRunningError,
 }
 
-pub async fn run() -> anyhow::Result<(), RunError> {
+pub async fn run() -> Result<(), RunError> {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
